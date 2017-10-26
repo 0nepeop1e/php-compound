29843e3051482bd9903d14b6ae5df602c4c987fe
0nepeop1e <engshun.lim@gmail.com>
2017-10-27 01:39:07 +0800

    add trait

--- src/CompoundProxy.php
+++ src/CompoundProxy.php
@@ -11,49 +11,5 @@ namespace onepeople\utils;
 
 class CompoundProxy extends Compound
 {
-    /**
-     * @var Compound
-     */
-    private $__parent;
-
-    /** @noinspection PhpMissingParentConstructorInspection */
-    /**
-     * @param $parent
-     * @param $offset
-     */
-    public function __construct($parent, $offset)
-    {
-        $this->__parent = $parent;
-        $this->__data = $offset;
-    }
-
-    public function set($value)
-    {
-        $internal = &$this->__parent->internalValue();
-        if (!is_array($internal)) {
-            $internal = [];
-        }
-        parent::set($value);
-    }
-
-    public function delete()
-    {
-        parent::delete();
-        unset($this->__parent[$this->__data]);
-    }
-
-    public function isPresent()
-    {
-        return isset($this->__parent[$this->__data]);
-    }
-
-    public function getParent()
-    {
-        return $this->__parent;
-    }
-
-    protected function &internalValue()
-    {
-        return $this->__parent->internalValue()[$this->__data];
-    }
+    use CompoundProxyTrait;
 }
